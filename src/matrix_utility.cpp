1f9fc99c4a1316d2aa045ba9f351061e664ea1df
Ching-Chuan Chen <zw12356@gmail.com>
2023-11-20 23:50:06 +0800

    add oneMKL support

--- src/matrix_utility.cpp
+++ src/matrix_utility.cpp
@@ -11,7 +11,7 @@
 // WITHOUT ANY WARRANTY; without even the implied warranty of
 // MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
 // GNU General Public License for more details.
-#include <RcppEigen.h>
+#include <oneMKL.h>
 
 SEXP cast_numeric(SEXP input) {
   if (!Rf_isReal(input)) {
