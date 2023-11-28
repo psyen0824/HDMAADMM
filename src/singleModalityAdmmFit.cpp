2e860d0c32b59bdb4d935866875adcdec9f01cc5
Ching-Chuan Chen <zw12356@gmail.com>
2023-11-28 17:48:13 +0800

    add elasticNetFit

--- src/singleModalityAdmmFit.cpp
+++ src/singleModalityAdmmFit.cpp
@@ -1,14 +1,6 @@
 #include <RcppEigen.h>
 #include <tuple>
-#include <iostream>
-
-inline int sgn(double val) {
-  return (val > 0.0) - (val < 0.0);
-}
-
-inline double softThreshold(double b, double lambda) {
-  return (std::abs(b) < lambda) ? 0.0 : sgn(b)*(std::abs(b) - lambda);
-}
+#include "utility.h"
 
 Eigen::MatrixXd upadteAlphaElasticNet(
     Eigen::MatrixXd alphaStep1,
@@ -167,13 +159,6 @@ Eigen::MatrixXd updateGammaFunc(
     gammaTemp(j, 0) = softThreshold(gammaTemp(j, 0), lambda1g);
   }
   return XtXInv * gammaTemp;
-}
-
-void printCoefficient(double *coef, std::string coefName, int numToPrint) {
-  int i;
-  for (i = 0; i < numToPrint; i++){
-    Rcpp::Rcout << ", " <<  coefName << "[" << i+1 << "]: " << coef[i];
-  }
 }
 
 // [[Rcpp::export]]
